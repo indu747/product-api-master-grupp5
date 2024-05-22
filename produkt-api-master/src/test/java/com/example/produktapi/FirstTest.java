91b91733570a875b5be3683413da7c24b99b0897
erik <erik.ostlind@gmail.com>
2024-05-22 15:49:23 +0200

    Added selenium support

---
+++ produkt-api-master/src/test/java/com/example/produktapi/FirstTest.java
@@ -0,0 +1,12 @@
+package com.example.produktapi;
+import org.junit.jupiter.api.*;
+import org.springframework.util.Assert;
+public class FirstTest {
+ //Skapar en första test Klass för att se till att projektet fungerar. Erik Östlind
+    @Test
+    @DisplayName("Make sure it works")
+    void doesThisWork(){
+       boolean isTrue = true;
+       Assertions.assertTrue(isTrue);
+    }
+}
