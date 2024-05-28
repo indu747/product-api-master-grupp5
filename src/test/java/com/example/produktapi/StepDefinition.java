3a0dc1256ac83921ae587714c01590e4edc51e5c
erik <erik.ostlind@gmail.com>
2024-05-28 18:11:25 +0200

    Lets see if it works

--- src/test/java/com/example/produktapi/StepDefinition.java
+++ src/test/java/com/example/produktapi/StepDefinition.java
@@ -7,12 +7,15 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class StepDefinition {
     WebDriver driver;
     @Before
     public void setup(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
