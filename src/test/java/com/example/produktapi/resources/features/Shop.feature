ca3723744cbd3636ada83e0b75f3f0301d4cdbea
Fppo02 <l_o_v__dogs@hotmail.com>
2024-06-10 23:57:04 +0200

    Sista Scenario är inte godkänt än men vill få upp övriga

--- src/test/java/com/example/produktapi/resources/features/Shop.feature
+++ src/test/java/com/example/produktapi/resources/features/Shop.feature
@@ -19,11 +19,43 @@ Feature: Shop
   | 2    |"https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg"|"White Gold Plated Princess"                                                  |"$ 9.99" |"Prinsessans bästa vän. Köp för att få den i din ägo"    |
   | 3    |"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"|"Pierced Owl Rose Gold Plated Stainless Steel Double"                         |"$ 10.99"|"Något med en uggla, och i guld och lite dubbel stål"    |
 
-  Scenario: Mens Clothing - Fjallraven
-   Given the user is on the homepage
+
+ Scenario: Mens Clothing - Fjallraven
+   Given the user is on mans clothing
    Then the title of the product should be "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
 
-   Scenario: Mens Clothing - Fjallraven pris
-    Given the user is on the homepage
-    Then the price should be "$ 109.95"
+
+ Scenario: Mens Clothing - Fjallraven pris
+  Given the user is on mans clothing
+  Then the price should be "$ 109.95"
+
+ Scenario: Mens Clothing - T-Shirt
+  Given the user is on mans clothing
+  Then the tittle of the product should be "Mens Casual Premium Slim Fit T-Shirts"
+
+ Scenario: : Mens Clothing - T-Shirt Price
+  Given the user is on mans clothing
+  Then the price of the t-shirt should be "$ 22.3"
+
+ Scenario: Mans Clothing - Jacket
+  Given the user is on mans clothing
+  Then the title should be "Mens Cotton Jacket"
+
+ Scenario: Mans Clothing - Jacket price
+  Given the user is on mans clothing
+  Then the price of the jacket should be "$ 55.99"
+
+ Scenario: Mans Clothing - Polo
+  Given the user is on mans clothing
+  Then the title of the polo should be "Mens Casual Slim Fit"
+
+ Scenario: Mans Clothing - Polo Price
+  Given the user is on mans clothing
+  Then the price of the polo should be "$ 15.99"
+
+ Scenario: Mans Clothing - Add items
+  Given the user is on mans clothing
+  When user clicks one item
+  Then the product eill be added to the checkout
+
 
--- src/test/java/com/example/produktapi/stepDefinitions/ShopStepDefinitions.java
+++ src/test/java/com/example/produktapi/stepDefinitions/ShopStepDefinitions.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -143,23 +144,68 @@ public class ShopStepDefinitions {
 
     //Patricia
 
-    @Given("the user is on the homepage")
-    public void the_user_is_on_the_homepage() {
+    @Given("the user is on mans clothing")
+    public void the_user_is_on_mans_clothing() {
         driver = new ChromeDriver();
         driver.get("https://webshop-agil-testautomatiserare.netlify.app/products#");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("card")));
     }
     @Then("the title of the product should be {string}")
     public void the_title_of_the_product_should_be(String expectedTitle) {
        String actualTitle = driver.findElement(By.xpath("(//h3[@class='card-title fs-4'])[1]")).getText();
     Assertions.assertEquals(expectedTitle,actualTitle);
     }
-
-
     @Then("the price should be {string}")
     public void the_price_should_be(String expectedPrice) {
         String actualPrice = driver.findElement(By.xpath("//strong[text()='$ 109.95']")).getText();
         Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+    @Then("the tittle of the product should be {string}")
+    public void the_tittle_of_the_product_should_be(String expectedTitle) {
+       String actualTitle = driver.findElement(By.xpath("(//h3[@class='card-title fs-4'])[2]")).getText();
+       Assertions.assertEquals(expectedTitle, actualTitle);
+    }
 
+    @Then("the price of the t-shirt should be {string}")
+    public void the_price_of_the_t_shirt_should_be(String expectedPrice) {
+    String actualPrice = driver.findElement(By.xpath("//strong[text()='$ 22.3']")).getText();
+    Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Then("the title should be {string}")
+    public void the_title_should_be(String expectedTitle) {
+        String actualTitile = driver.findElement(By.xpath("(//h3[@class='card-title fs-4'])[3]")).getText();
+        Assertions.assertEquals(expectedTitle, actualTitile);
+    }
+    @Then("the price of the jacket should be {string}")
+    public void the_price_of_the_jacket_should_be(String expectedPrice) {
+        String actualPrice = driver.findElement(By.xpath("//strong[text()='$ 55.99']")).getText();
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+    @Then("the title of the polo should be {string}")
+    public void the_title_of_the_polo_should_be(String expectedTitile) {
+        String actualTitile = driver.findElement(By.xpath("//h3[text()='Mens Casual Slim Fit']")).getText();
+        Assertions.assertEquals(expectedTitile, actualTitile);
+    }
+    @Then("the price of the polo should be {string}")
+    public void the_price_of_the_polo_should_be(String expectedPrice) {
+        String actualPrice = driver.findElement(By.xpath("//strong[text()='$ 15.99']")).getText();
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+    @When("user clicks one item")
+    public void user_clicks_one_item() {
+        driver.findElement(By.xpath("(//button[@class='btn btn-primary'])[1]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("card")));
+    }
+    @Then("the product eill be added to the checkout")
+    public void the_product_eill_be_added_to_the_checkout() {
+        driver.findElement(By.xpath("//a[.='🛒 Checkout 1']")).click();
+        driver.findElement(By.xpath("(//li[contains(@class,'list-group-item d-flex')])[1]")).getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("card")));
+        Assertions.assertTrue(true);
 
     }
 
