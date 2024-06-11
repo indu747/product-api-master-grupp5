6e3c0b7315147cb533fe468aa837a602d93af999
erik <erik.ostlind@gmail.com>
2024-06-11 18:27:26 +0200

    Made some tiny tests to combat the larger outline scenario

--- src/test/java/com/example/produktapi/stepDefinitions/ErikStepDefinitions.java
+++ src/test/java/com/example/produktapi/stepDefinitions/ErikStepDefinitions.java
@@ -10,19 +10,110 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
-public class ShopStepDefinitions {
-
+public class ErikStepDefinitions {
     private WebDriver driver;
 
-    public ShopStepDefinitions() {
+    public ErikStepDefinitions() {
         this.driver = SetupHooks.getDriver();
     }
+
+    private static final Map<String, String> PAGE_URLS = Map.of(
+            "homepage", "https://webshop-agil-testautomatiserare.netlify.app/",
+            "shop_page", "https://webshop-agil-testautomatiserare.netlify.app/products",
+            "checkout_page", "https://webshop-agil-testautomatiserare.netlify.app/checkout",
+            "about_page", "https://webshop-agil-testautomatiserare.netlify.app/about"
+    );
+
+
     //Erik Östlind
-    @Given("User is on {string}")
-    public void user_is_on(String shopPage) {
+    @Given("user is on {string}")
+    public void user_is_on(String pageName) {
+        String url = PAGE_URLS.get(pageName);
+        if (url != null) {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            driver.get(url);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("header")));
+        } else {
+            throw new IllegalArgumentException("Unknown page name: " + pageName);
+        }
+    }
+
+    @Then("navbar has {int} links")
+    public void navbar_has_links(Integer int1) {
+        WebElement navBar = driver.findElement(By.tagName("header"));
+        //Put them in a list
+        List<WebElement> navBarLinks  = navBar.findElements(By.tagName("a"));
+        //Get the size of navbar
+        int amountOfLinks = navBarLinks.size();
+        Assertions.assertEquals(int1,amountOfLinks);
+    }
+
+    //Erik Östlind
+    @Then("the {int} contains a {string}")
+    public void the_contains_a(Integer nthItem, String linkText) {
+        //Get navbar element
+        WebElement navBar = driver.findElement(By.tagName("header"));
+        //Put them in a list
+        List<WebElement> navBarLinkList  = navBar.findElements(By.tagName("a"));
+        //Get the nth item
+        WebElement specificNavBarElement = navBarLinkList.get(nthItem);
+        //Get the link text
+        String navBarItemText = specificNavBarElement.getText();
+        //Assert on text
+        Assertions.assertEquals(linkText,navBarItemText );
+
+    }
+    //Erik Östlind
+    @Then("the {int} contain link to {string}")
+    public void the_contain_link_to(Integer nthItem, String href) {
+        //Get navbar element
+        WebElement navBar = driver.findElement(By.tagName("header"));
+        //Put them in a list
+        List<WebElement> navBarLinkList  = navBar.findElements(By.tagName("a"));
+        //Get the nth item
+        WebElement specificNavBarElement = navBarLinkList.get(nthItem);
+        //Get the specific href
+        String navBarItemHref = specificNavBarElement.getAttribute("href");
+        //Assert on Href
+        Assertions.assertEquals(href,navBarItemHref);
+
+    }
+    //Erik Östlind
+    @When("user clicks on the {int}")
+    public void user_clicks_on_the(Integer nthItem) {
+        //Get navbar element
+        WebElement navBar = driver.findElement(By.tagName("header"));
+        //Put them in a list
+        List<WebElement> navBarLinkList  = navBar.findElements(By.tagName("a"));
+        //Get the nth item
+        WebElement specificNavBarElement = navBarLinkList.get(nthItem);
+        specificNavBarElement.click();
+    }
+    //Erik Östlind
+    @Then("user should get taken to {string}")
+    public void user_should_get_taken_to(String href) {
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(href));
+    }
+    //Erik Östlind
+    @Given("user is on Shop Page")
+    public void user_is_on_shop_page() {
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app/products");
+    }
+    //Erik Östlind
+    @Given("user is on Checkout Page")
+    public void user_is_on_checkout_page() {
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app/checkout");
+    }
+
+    //Erik Östlind
+    @Given("User is on page {string}")
+    public void user_is_on_page(String shopPage) {
         driver.get(shopPage);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("card")));
@@ -54,13 +145,13 @@ public class ShopStepDefinitions {
     //Erik Östlind
     @Then("the {int} card should have {string} as image link")
     public void the_card_should_have_as_image_link(Integer nthCard, String expectedImageLink) {
-    List <WebElement> productCards = driver.findElements(By.className("card"));
-    WebElement specificProductCard = productCards.get(nthCard);
-    //Get the image from nth card
-    WebElement image = specificProductCard.findElement(By.tagName("img"));
-    //Get the image link and assert on it
-    String imageLink = image.getAttribute("src");
-    Assertions.assertEquals(expectedImageLink,imageLink);
+        List <WebElement> productCards = driver.findElements(By.className("card"));
+        WebElement specificProductCard = productCards.get(nthCard);
+        //Get the image from nth card
+        WebElement image = specificProductCard.findElement(By.tagName("img"));
+        //Get the image link and assert on it
+        String imageLink = image.getAttribute("src");
+        Assertions.assertEquals(expectedImageLink,imageLink);
 
     }
     //Erik Östlind
@@ -135,10 +226,8 @@ public class ShopStepDefinitions {
         List <WebElement> productCards = driver.findElements(By.className("card"));
         WebElement specificProductCard = productCards.get(nthCard);
         //Get the add to card button inside specific card
-       String actualButtonText = specificProductCard.findElement(By.tagName("button")).getText();
-       //Assert button text against expected text
+        String actualButtonText = specificProductCard.findElement(By.tagName("button")).getText();
+        //Assert button text against expected text
         Assertions.assertEquals(expectedButtonText,actualButtonText);
     }
-
-
 }
